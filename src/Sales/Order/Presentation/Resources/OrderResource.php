6417f4b5a7497e1de45e588cb0a32377506d9c79
Rodrigo Paco <rodrigopaco.1986@gmail.com>
2025-05-10 18:37:34 -0400

    testing

--- src/Sales/Order/Presentation/Resources/OrderResource.php
+++ src/Sales/Order/Presentation/Resources/OrderResource.php
@@ -31,7 +31,7 @@ class OrderResource extends JsonResource
                 'currency' => $order->getCurrency(),
                 'total' => $order->getTotal(),
                 'items' => new OrderItemCollection($order->getItems()),
-                'testing' => 'testing',
+                'demo' => 'demo',
             ],
         ];
     }
