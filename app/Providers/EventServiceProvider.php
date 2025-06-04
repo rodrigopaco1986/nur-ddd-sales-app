8e6924e73ec8dd1399b43c1567d275ef7f3d046b
Rodrigo Paco <rodrigopaco.1986@gmail.com>
2025-06-03 22:29:28 -0400

    fixed docker compose and enabled job queues

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -4,22 +4,32 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Src\Sales\Invoice\Application\Listeners\CreateOrderInvoice;
+use Src\Sales\Order\Application\Events\OrderCreatedIntegrationEvent;
+use Src\Sales\Order\Application\Jobs\PublishOrderCreatedToBrokerJob;
+use Src\Sales\Order\Application\Listeners\DispatchOrderCreatedIntegrationEvent;
 use Src\Sales\Order\Domain\Events\OrderCreatedEvent;
 use Src\Sales\Payment\Application\Listeners\CreateOrderPaymentSchedules;
 
 class EventServiceProvider extends ServiceProvider
 {
     protected $listen = [
+        // DOMAIN EVENTS
         OrderCreatedEvent::class => [
             CreateOrderPaymentSchedules::class,
             CreateOrderInvoice::class,
+            DispatchOrderCreatedIntegrationEvent::class,
         ],
-        //InvoiceCreatedEvent::class => [
+        // InvoiceCreatedEvent::class => [
         //    SendEmailInvoice::class,
-        //],
-        //PaymentRegisteredEvent::class => [
+        // ],
+        // PaymentRegisteredEvent::class => [
         //    NotifyPaymentRegistered::class,
-        //],
+        // ],
+
+        // INTEGRATION EVENTS
+        OrderCreatedIntegrationEvent::class => [
+            PublishOrderCreatedToBrokerJob::class,
+        ],
     ];
 
     public function boot()
